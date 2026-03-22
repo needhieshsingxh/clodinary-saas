efec7ca9e35355bb1bcac174bb292652b4feaead
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-03-22 09:29:31 +0530

    Refactor Prisma client import and update schema generator configuration

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,10 @@
-export interface Video{
-    id: String
-    title: String
-    description: String
-    publicId: String
-    originalSize: number
-    compressedSize: number
-    duration: number
-    createdAt: Date
+export interface Video {
+  id: string;
+  title: string;
+  description: string | null;
+  publicId: string;
+  originalSize: string;
+  compressedSize: string;
+  duration: number;
+  createdAt: string | Date;
 }
